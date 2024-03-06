5b859fa9f9d2736d6461a168eb3a6fcea670bbdc
rueueue <146345327+rueueue@users.noreply.github.com>
2024-03-06 17:46:53 +0800

    updated button and icon

--- sample/pages/recipe.js
+++ sample/pages/recipe.js
@@ -2,10 +2,21 @@ function RecipePage(){
     return(
    <>
    <>
+   <>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="recipe_styles.css" />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+    integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+    crossOrigin="anonymous"
+    referrerPolicy="no-referrer"
+  />
   <title>Recipes</title>
   <div className="navbar">
-    <div className="img1-arrow">
-      <img src="img/back.png" alt="" />
+    <div className="arrow">
+      <i className="fa-solid fa-chevron-left icon" />
     </div>
     <div className="img2-logo">
       <img src="img/logo.png" alt="" />
@@ -164,9 +175,11 @@ function RecipePage(){
   </div>
   <div className="browse">
     <div className="browse-btn">
-      <h4>Browse more recipes</h4>
+      <button>Browse more recipes</button>
     </div>
   </div>
+</>
+
 </>
 
         </>
